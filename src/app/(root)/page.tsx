902db74bbfd9caae1f4163523881b3b8b9ce9c7a
Rajdip-Pal <rajdippal.rp@outlook.com>
2025-03-16 17:29:46 +0530

    Base Components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,22 +1,32 @@
-'use client'; // eslint-disable-line
+'use client';
 
 // importing needed modules
 import React from 'react';
+import { useRouter } from 'next/router';
 
 // importing needed components
 import Home from '@/components/Home/Home';
 import Navbar from '@/components/Navbar/Navbar';
+import Hero from '@/components/Hero_Landing/Hero';
 
 const page = () => {
+  const [showingHero, setShowingHero] = React.useState(true);
+
   React.useEffect(() => {
     document.title = 'Slay Ctf';
+    setTimeout(() => setShowingHero(false), 4000);
   });
 
   return (
-    <div className='justify-center items-center'>
-      <Navbar />
-      {/* <Home /> */}
-    </div>
+    <React.Fragment>
+      {showingHero && <Hero />}
+      {!showingHero && (
+        <div className='justify-center items-center'>
+          <Navbar />
+          {/* <Home /> */}
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='flex justify-center items-center h-screen font-squidGame'>
-      About
+    <div className='flex justify-center items-center h-screen font-squidGame text-white'>
+      About Page
     </div>
   );
 };
