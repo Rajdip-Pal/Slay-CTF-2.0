2f804c4f98f9924c8a413e596b622d88a978bc5e
sarbik <sarbikchatterjee99@gmail.com>
2025-03-18 02:06:39 +0530

    added some animations on the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { ThemeContext } from '@/contexts/ThemeProvider';
-import Rules from '@/components/Rules'
+"use client";
+import React from "react";
+import { ThemeContext } from "@/contexts/ThemeProvider";
+import Rules from "@/components/Rules";
+import { motion } from "framer-motion";
+
 function Home() {
   const { theme } = React.useContext(ThemeContext);
 
   return (
     <>
-    <div className={`min-h-screen flex items-center justify-center ${theme === 'dark' ? 'text-white' : 'text-black'}`}>
-      <img
-        src="/images/slayy.png"
-        alt="Slay CTF Logo"
-        className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg w-full h-auto"
-      />
-    </div>
-    <Rules/>
+      <div
+        className={`min-h-screen flex flex-col items-center justify-center text-center ${
+          theme === "dark" ? "text-white" : "text-black"
+        }`}
+      >
+        <motion.img
+          src="/images/slayy.png"
+          alt="Slay CTF Logo"
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.2, duration: 1 }}
+          className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg w-full h-auto mb-4"
+        />
+        <div className="mt-40 typing-container">
+          <p className="text-2xl sm:text-xl md:text-3xl lg:text-4xl font-bold w-full text-center text-white border border-pink-500 rounded px-4 py-2">
+            <span className="typing">Let the game begin.</span>
+          </p>
+        </div>
+      </div>
+
+      <Rules />
     </>
   );
 }
