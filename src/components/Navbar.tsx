c2cd1b10abe50a7207e0fe3ef048c5f97956833d
Rajdip-Pal <rajdippal.rp@outlook.com>
2025-04-02 03:56:08 +0530

    basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button';
 
 function Navbar() {
   return (
-    <nav className=' from-black  via-[rgba(0,0,0,0.5)] to-transparent flex justify-between p-5 text-white'>
+    <nav className='fixed w-[100%] z-10 from-black via-[rgba(0,0,0,0.5)] to-transparent backdrop-blur-xl flex justify-between p-5 text-white'>
       <div className='h-10 font-bold text-3xl'>SLAY CTF</div>
       <div className='bg-transparent'>
         <NavigationMenuDemo />
